31c1cf4a570554d6cc9101e3e77c55ab95cbc372
Jaroslavna Kuptsova <55083918+ykuptsova@users.noreply.github.com>
2023-01-26 16:55:12 +0200

    udpates

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const auth = require('../middlewares/auth');
 const { getUser, updateProfile } = require('../controllers/users');
 const {
-  validateLogin,
+  validateUpdateUser,
 } = require('../utils/validations');
 
 router.use(auth);
-router.get('/me', validateLogin, getUser);
-router.patch('/me', validateLogin, updateProfile);
+router.get('/me', getUser);
+router.patch('/me', validateUpdateUser, updateProfile);
 
 module.exports = router;
